a016c7012a8ff930b7549b5f1e770d39bc533263
Muhammad Sharaf <msherf98@gmail.com>
2024-12-17 17:44:02 +0100

    added new features to MyCollate function

--- data_loaders/padding_collate.py
+++ data_loaders/padding_collate.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn.utils.rnn import pad_sequence
 
 
@@ -8,7 +9,8 @@ class MyCollate:
         self.pad_idx = pad_idx
 
     def __call__(self, batch):
-        # {
+        #    Input batch is
+        # [ {
         #     "id": statement_id,
         #     "barely_true_count": barely_true_count,
         #     "false_count": false_count,
@@ -23,25 +25,66 @@ class MyCollate:
         #     "state_info": state_info_tokens,
         #     "party_affiliation": party_affiliation_tokens,
         #     "context_venue_or_location": context_venue_or_location_tokens,
-        # }
+        # } ]
 
         # Batch is a tuple of sentences and their respective labels, this can be changed depending on the input fields we are using for training
         # by changing the get item function in the dataset class.
+
+        # numerical counts
+        statement_ids = [x["id"] for x in batch]
         barely_true_counts = [x["barely_true_count"] for x in batch]
         false_counts = [x["false_count"] for x in batch]
         half_true_counts = [x["half_true_count"] for x in batch]
         mostly_true_counts = [x["mostly_true_count"] for x in batch]
         pants_on_fire_counts = [x["pants_on_fire_count"] for x in batch]
+
+        # tokenized text
         statements = [x["statement"] for x in batch]
         subjects = [x["subject"] for x in batch]
         speakers = [x["speaker_"] for x in batch]
         speaker_job_titles = [x["speaker_job_title"] for x in batch]
-        labels = [x["label"] for x in batch]
         state_infos = [x["state_info"] for x in batch]
         party_affiliations = [x["party_affiliation"] for x in batch]
         context_venue_or_locations = [x["context_venue_or_location"] for x in batch]
-        paded_sentences = pad_sequence(
-            sentences, batch_first=True, padding_value=self.pad_idx
+
+        labels = [x["label"] for x in batch]
+
+        # Pad text sequences
+        paded_statements = pad_sequence(
+            statements, batch_first=True, padding_value=self.pad_idx
+        )
+        paded_subjects = pad_sequence(
+            subjects, batch_first=True, padding_value=self.pad_idx
+        )
+        paded_speakers = pad_sequence(
+            speakers, batch_first=True, padding_value=self.pad_idx
+        )
+        paded_speaker_job_titles = pad_sequence(
+            speaker_job_titles, batch_first=True, padding_value=self.pad_idx
+        )
+        paded_state_infos = pad_sequence(
+            state_infos, batch_first=True, padding_value=self.pad_idx
+        )
+        paded_party_affiliations = pad_sequence(
+            party_affiliations, batch_first=True, padding_value=self.pad_idx
+        )
+        paded_context_venue_or_locations = pad_sequence(
+            context_venue_or_locations, batch_first=True, padding_value=self.pad_idx
         )
 
-        return paded_sentences, torch.tensor(labels)
+        return {
+            "id": torch.tensor(statement_ids),
+            "barely_true_count": torch.tensor(barely_true_counts),
+            "false_count": torch.tensor(false_counts),
+            "half_true_count": torch.tensor(half_true_counts),
+            "mostly_true_count": torch.tensor(mostly_true_counts),
+            "pants_on_fire_count": torch.tensor(pants_on_fire_counts),
+            "statement": paded_statements,
+            "subject": paded_subjects,
+            "speaker": paded_speakers,
+            "speaker_job_title": paded_speaker_job_titles,
+            "state_info": paded_state_infos,
+            "party_affiliation": paded_party_affiliations,
+            "context_venue_or_location": paded_context_venue_or_locations,
+            "label": labels,
+        }
